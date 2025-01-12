b31e6ca3ea85215751320fb69fd43437a41a71f0
박진영 <am030813@naver.com>
2025-01-12 21:51:49 +0900

    FEAT: 구글, 네이버, 카카오 OAuth 추가, TEST 필요

--- src/main/java/com/secondserve/SecondServeApplication.java
+++ src/main/java/com/secondserve/SecondServeApplication.java
@@ -1,7 +1,6 @@
 package com.secondserve;
 
 import org.springframework.boot.SpringApplication;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
--- src/main/java/com/secondserve/repository/AppointmentRepository.java
+++ src/main/java/com/secondserve/repository/AppointmentRepository.java
@@ -1,9 +1,7 @@
 package com.secondserve.repository;
 
 import com.secondserve.entity.Appointment;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
